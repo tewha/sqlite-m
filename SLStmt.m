3e548e6cb6420ba6d53a532d93e574d04c385946
Steven Fisher <tewha@me.com>
2009-07-20 20:37:18 +0000

    Add space.

--- SLStmt.m
+++ SLStmt.m
@@ -18,24 +18,25 @@
 	return _err & 0xFF;
 }
 
-- (void)setResult:(int)err {
+- (void)setResult: (int)err {
 	_err = err;
 	_msg = sqlite3_errmsg([_database dtbs]);
 	if ( ( _err != SQLITE_OK ) && ( self.simpleErr < 100 ) )
 		NSLog( @"SLStmt: (%d) %s", _err, _msg );
 }
 
-+ (id)stmtWithDatabase:(SLDatabase*)database
-				   sql:(NSString*)sql {
-    return [[[self alloc] initWithDatabase:database sql:sql] autorelease];
++ (id)stmtWithDatabase: (SLDatabase*)database
+				   sql: (NSString*)sql {
+    return [[[self alloc] initWithDatabase: database
+									   sql: sql] autorelease];
 }
 
-- (id)initWithDatabase:(SLDatabase*)database
-				   sql:(NSString*)sql {
+- (id)initWithDatabase: (SLDatabase*)database
+				   sql: (NSString*)sql {
 	self = [super init];
 	if (!self) return self;
 	_database = [database retain];
-	[self prepareSql:sql];
+	[self prepareSql: sql];
 	return self;
 }
 
@@ -46,12 +47,12 @@
 	[super dealloc];
 }
 
-- (SLStmt*)prepareSql:(NSString*)sql {
+- (SLStmt*)prepareSql: (NSString*)sql {
 	[sql retain];
 	[self close];
 	[_sql release];
 	_sql = sql;
-	[self setResult:sqlite3_prepare_v2([_database dtbs], [_sql UTF8String], -1, &_stmt, &_nextSql)];
+	[self setResult: sqlite3_prepare_v2([_database dtbs], [_sql UTF8String], -1, &_stmt, &_nextSql)];
 	_bind = 0;
 	return self;
 }
@@ -59,7 +60,7 @@
 - (SLStmt*)prepareNext {
 	if ( ( _nextSql == NULL ) || ( *_nextSql == 0 ) )
 		return nil;
-	[self setResult:sqlite3_prepare_v2([_database dtbs], _nextSql, -1, &_stmt, &_nextSql)];
+	[self setResult: sqlite3_prepare_v2([_database dtbs], _nextSql, -1, &_stmt, &_nextSql)];
 	return self;
 }
 
@@ -88,7 +89,7 @@
 }
 
 - (SLStmt*)step {
-	[self setResult:sqlite3_step( _stmt )];
+	[self setResult: sqlite3_step( _stmt )];
 	_column = 0;
 	return ( [self simpleErr] == SQLITE_ROW ) ? self : nil;
 }
@@ -97,43 +98,43 @@
 	return sqlite3_column_count( _stmt );
 }
 
-- (NSString*)columnName:(int)column {
+- (NSString*)columnName: (int)column {
 	const char *text = sqlite3_column_name( _stmt, column );
 	if ( text == NULL )
 		return nil;
-	return [NSString stringWithUTF8String:text];
+	return [NSString stringWithUTF8String: text];
 }
 
 - (NSString*)columnName {
 	const char *text = sqlite3_column_name( _stmt, _column );
 	if ( text == NULL )
 		return nil;
-	return [NSString stringWithUTF8String:text];
+	return [NSString stringWithUTF8String: text];
 }
 
-- (long long)longLongValue:(int)column {
+- (long long)longLongValue: (int)column {
 	return sqlite3_column_int64( _stmt, column );
 }
 
 - (long long)longLongValue {
-	return [self longLongValue:_column++];
+	return [self longLongValue: _column++];
 }
 
-- (NSString*)stringValue:(int)column {
+- (NSString*)stringValue: (int)column {
 	const char *text = (char*)sqlite3_column_text( _stmt, column);
 	if ( text == NULL )
 		return nil;
-	return [NSString stringWithUTF8String:text];
+	return [NSString stringWithUTF8String: text];
 }
 
 - (NSString*)stringValue {
 	const char *text = (char*)sqlite3_column_text( _stmt, _column++);
 	if ( text == NULL )
 		return nil;
-	return [NSString stringWithUTF8String:text];
+	return [NSString stringWithUTF8String: text];
 }
 
-- (int)columnType:(int)column {
+- (int)columnType: (int)column {
 	return sqlite3_column_type( _stmt, column );
 }
 
@@ -141,78 +142,78 @@
 	return sqlite3_column_type( _stmt, _column );
 }
 
-- (id)value:(int)column {
+- (id)value: (int)column {
 	int type = sqlite3_column_type( _stmt, column );
 	switch (type) {
 		case SQLITE_INTEGER:
-			return [NSNumber numberWithLongLong:sqlite3_column_int64( _stmt, column )];
+			return [NSNumber numberWithLongLong: sqlite3_column_int64( _stmt, column )];
 		case SQLITE_FLOAT:
-			return [NSNumber numberWithDouble:sqlite3_column_double( _stmt, column )];
+			return [NSNumber numberWithDouble: sqlite3_column_double( _stmt, column )];
 		case SQLITE_BLOB: {
 			const void *bytes = sqlite3_column_blob( _stmt, column );
-			return [NSData dataWithBytes:bytes
-								  length:sqlite3_column_bytes( _stmt, column )];
+			return [NSData dataWithBytes: bytes
+								  length: sqlite3_column_bytes( _stmt, column )];
 		}
 		case SQLITE_NULL:
 			return nil;
-		case SQLITE_TEXT: {
-			const unsigned char *text = sqlite3_column_text( _stmt, column );
-			return [NSString stringWithUTF8String:(char*)text];
-		}
+		case SQLITE_TEXT:
+			return [NSString stringWithUTF8String: (char*)sqlite3_column_text( _stmt, column ) ];
 		default:
 			return nil;
 	}
 }
 
 - (id)value {
-	return [self value:_column++];
+	return [self value: _column++];
 }
 
 - (NSDictionary*)allValues {
 	NSMutableDictionary *temp_values = [[NSMutableDictionary alloc] init];
 	int n = [self columnCount];
 	for ( int i = 0; i < n; i++ ) {
-		id value = [self value:i];
+		id value = [self value: i];
 		if (!value)
 			continue;
-		NSString *name = [self columnName:i];
-		[temp_values setObject:value forKey:name];
+		NSString *name = [self columnName: i];
+		[temp_values setObject: value
+						forKey: name];
 	}
 	NSDictionary *values = [NSDictionary dictionaryWithDictionary: temp_values];
 	[temp_values release];
 	return values;
 }
 
-- (SLStmt*)bindLongLong:(long long)value
-			   forIndex:(int)index {
-	[self setResult:sqlite3_bind_int64( _stmt, index+1, value )];
+- (SLStmt*)bindLongLong: (long long)value
+			   forIndex: (int)index {
+	[self setResult: sqlite3_bind_int64( _stmt, index+1, value )];
 	return self;
 }
 
-- (SLStmt*)bindLongLong:(long long)value {
-	[self bindLongLong:value forIndex:_bind++];
+- (SLStmt*)bindLongLong: (long long)value {
+	return [self bindLongLong: value
+					 forIndex: _bind++];
+}
+
+- (SLStmt*)bindString: (NSString*)value
+			 forIndex: (int)index {
+	[self setResult: sqlite3_bind_text( _stmt, index+1, [value UTF8String], -1, SQLITE_TRANSIENT )];
 	return self;
 }
 
-- (SLStmt*)bindString:(NSString*)value
-			 forIndex:(int)index {
-	[self setResult:sqlite3_bind_text( _stmt, index+1, [value UTF8String], -1, SQLITE_TRANSIENT )];
+- (SLStmt*)bindString: (NSString*)value {
+	return [self bindString: value
+				   forIndex: _bind++];
+}
+
+- (SLStmt*)bindData: (NSData*)value
+		   forIndex: (int)index {
+	[self setResult: sqlite3_bind_blob( _stmt, index+1, [value bytes], [value length], SQLITE_TRANSIENT )];
 	return self;
 }
 
-- (SLStmt*)bindString:(NSString*)value {
-	[self bindString:value forIndex:_bind++];
-	return self;
-}
-
-- (SLStmt*)bindData:(NSData*)value
-		   forIndex:(int)index {
-	[self setResult:sqlite3_bind_blob( _stmt, index+1, [value bytes], [value length], SQLITE_TRANSIENT )];
-	return self;
-}
-
-- (SLStmt*)bindData:(NSData*)value {
-	return [self bindData:value forIndex:_bind++];
+- (SLStmt*)bindData: (NSData*)value {
+	return [self bindData: value
+				 forIndex: _bind++];
 }
 
 
